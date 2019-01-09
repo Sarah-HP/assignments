1f4a870bd6916e18476300012c09cf4e77562270
SARAH POLLNOW <SPOLLNOW@GSE.HARVARD.EDU>
2019-01-09 16:28:23 -0500

    add js calculator

---
+++ calculator.js
@@ -0,0 +1,17 @@
+# Make a JavaScript fuction that can add, subtract, etc.
+
+function add(a,b) {
+	return a+b
+}
+
+function subtract(a,b) {
+	return a-b
+}
+
+function multiply(a,b) {
+	return a*b
+}
+
+function divide(a,b) {
+	return a/b
+}
