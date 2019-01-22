761a9ed707b44f1fd7b418e7939e0bd85f799465
SARAH POLLNOW <SPOLLNOW@GSE.HARVARD.EDU>
2019-01-21 23:01:38 -0500

    group so that I print to vegetables_by_color.json

--- pythondata/grouping.py
+++ pythondata/grouping.py
@@ -4,12 +4,13 @@ import json
 from pprint import pprint
 
 
-with open('vegetables.csv', 'r') as f:
-    reader = csv.DictReader(f)
-    rows = [dict(row) for row in reader] # Convert Ordered Dict to regular dict (python 3.6 or higher)
+# with open('vegetables.csv', 'r') as f:
+#     reader = csv.DictReader(f)
+#     rows = [dict(row) for row in reader] # Convert Ordered Dict to regular dict (python 3.6 or higher)
+# # pprint(f)
+# # vegetables = list(rows)
 
-vegetables = str(rows)
-#Read vegtables.csv into a variable called vegtables.
+# #Read vegtables.csv into a variable called vegtables.
 	# Blocked here - I've tried a number of variations of code (some of which are shown below), but with no success
 		# # Read vegetables.csv into a variable called vegetables.
 		# import csv
@@ -26,16 +27,33 @@ vegetables = str(rows)
 		#    vegetables = list(reader)
 		# pprint(vegetables)
 		# # print(rows)
-pprint(vegetables)
+# I was struggling with the CSV, so I'm just going to bring in the dataframe
 
+vegetables = [
+ {"name": "eggplant", "color": "purple"},
+ {"name": "tomato", "color": "red"},
+ {"name": "corn", "color": "yellow"},
+ {"name": "okra", "color": "green"},
+ {"name": "arugula", "color": "green"},
+ {"name": "broccoli", "color": "green"},
+]
+
+# Make an empty dictionary
 vegetables_by_color = {}
+# loop through the "vegetables" list of dictionaries
 for veg in vegetables:
-	color = veg["color"]
-	if veg in vegetables_by_color:
-		vegetables_by_color[veg]+=1
+	color = veg['color']
+	if color in vegetables_by_color:
+		vegetables_by_color[color].append(veg)
 	else:
-		vegetables_by_color[veg] = 1
+		vegetables_by_color[color] = [veg]
+#Print things out
 pprint(vegetables_by_color)
 
 
-# source consulted - https://stackoverflow.com/questions/35558053/load-csv-file-into-list
+# Output vegetables_by_color into a json
+with open('vegetables_by_color.json', 'w') as f:
+    json.dump(vegetables_by_color, f)
+
+
+# # source consulted - https://stackoverflow.com/questions/35558053/load-csv-file-into-list, http://sthurlow.com/python/lesson06/
