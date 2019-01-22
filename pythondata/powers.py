d0f36c547a56ad5bf325b9303b2b2461426b2237
SARAH POLLNOW <SPOLLNOW@GSE.HARVARD.EDU>
2019-01-21 21:58:30 -0500

    try it from python/filtering (topic: veggies)

--- pythondata/powers.py
+++ pythondata/powers.py
@@ -14,17 +14,12 @@ allpowers = []
 
 # First we need a list of members
 members = squad['members']  # For this entire dictionary of squad, this accesses the key members
+
+# Loop through the members of the squad and append the powers of each member to the powers array.
 for member in members:
 	powers = member['powers']
 	for power in powers:
 		allpowers.append(power)
 
-print(allpowers)
-
-
-
-
-# Loop through the members of the squad and append the powers of each member to the powers array.
-
-
 # Print each power to the terminal.
+print(allpowers)
